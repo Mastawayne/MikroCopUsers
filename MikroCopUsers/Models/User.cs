26accfcf4039c7027cad2cb274ab11f98ff5c4d0
Mastawayne <taranisdamir11@gmail.com>
2025-06-12 00:41:30 +0200

    Updated Users file

--- MikroCopUsers/Models/User.cs
+++ MikroCopUsers/Models/User.cs
@@ -1,21 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace MikroCopUsers.Models
+namespace GmiBetting.Models
 {
     public class User
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        public string UserName { get; set; }
-
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string MobileNumber { get; set; }
-        public string Language { get; set; }
-        public string Culture { get; set; }
-
-        [Required]
-        public string PasswordHash { get; set; }
+        public required string UserName { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? Language { get; set; }
+        public string? Culture { get; set; }
+        public required string PasswordHash { get; set; }
     }
 }
